2afd1017666ba18072ba2d73b7223d8d56a26f35
gldanoob <caleblolhk@gmail.com>
2022-11-28 21:59:25 +0000

    navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,43 +1,45 @@
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import data from '../assets/data.json';
 import '../style/App.css';
 import Footer from './Footer';
-import Header from './Header';
-import Section from './Section';
+import Link from './Link';
+import Module from './Module';
+import Menu from './Menu'
+
 
 function App() {
-
-	const [progress, setProgress] = useState(0);
-	useEffect(() => {
-		const i = setInterval(t => {
-			setProgress(progress => (progress + 1) % 100)
-		}, 16)
-
-		return () => { clearInterval(i) }
-
-	}, [])
-
-	data.sections[0].tasks[0].score = progress;
-	data.sections[1].tasks[2].score = (progress / 5) | 0;
-
+	const defaultModule = data.modules[0]
 	return (
-		<div className="App">
-			<div className="App-body">
-				<div className='content'>
-					<Header desc={data.desc}>
-						{data.header}
-					</Header>
-					{
-						data.sections.map(
-							(section, i) => <Section name={section.name} desc={section.desc} tasks={section.tasks} key={i}/>
-						)
-					}
+		<BrowserRouter>
+			<div className="App">
+				<div className="App-body">
+					<Menu modules={data.modules.map(m => m.name)}></Menu>
+					<div className='content'>
+						<Routes>
+							{
+								data.modules.map(
+										(module, i) =>
+										<Route path={'/' + module.name} element={
+											<Module name={module.name} desc={module.desc} sections={module.sections}></Module>
+										} key={i}>
+										</Route>
+								)
+							}
+
+							<Route path="/" element={
+								<Module name={defaultModule.name} desc={defaultModule.desc} sections={defaultModule.sections}></Module>
+							}></Route>
+
+							<Route path="*" element={<div>MODULE NOT FOUND</div>}></Route>
+
+						</Routes>
+					</div>
+					<Footer>
+						<Link url="https://github.com/gldanoob">@gldanoob</Link>
+					</Footer>
 				</div>
-				<Footer>
-					@gldanoob
-				</Footer>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
