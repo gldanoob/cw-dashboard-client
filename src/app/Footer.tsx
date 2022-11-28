26767f3ddc5594f400dd1bdc69235adf7ca262d2
gldanoob <caleblolhk@gmail.com>
2022-11-28 21:59:38 +0000

    typing

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from 'react'
 import '../style/Footer.css'
 
 interface Props {
-    children: string
+    children: ReactNode
 }
 
 export default function Footer(props: Props) {
